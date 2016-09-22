c30a30e4003a7fb4f99db5711041efef49e7684b
Ben Allen <ben.allen2@gmail.com>
2016-09-22 14:57:31 -0400

    mostly functional interface

--- index.js
+++ index.js
@@ -2,27 +2,47 @@ var menuOpen = false;
 
 var projects = [{
   name: "Metro Better",
-  info: "A project that displays information the next incoming train to any Metro station in the Washington D.C. Metro Area. I made this project for myself to practice pulling information in real-time from a large scale API. The project uses HTML, CSS, Javascript, jQuery and Bootstrap."
+  info: "A project that displays information the next incoming train to any Metro station in the Washington D.C. Metro Area. I made this project for myself to practice pulling information in real-time from a large scale API. The project uses HTML, CSS, Javascript, jQuery and Bootstrap.",
+  img: "./metross.png",
+  href: "./Metro/Metro.html"
 }, {
   name: "Twitch Viewer",
-  info: "This project came as a challenge from Free Code Camp to use the Twitch.tv API to create a page on which users could see if certain featured streamers were currently online.  I decided to expand on it by embedding a video player directly into the site and including the top 10 currently online streamers for Starcraft II."
+  info: "This project came as a challenge from Free Code Camp to use the Twitch.tv API to create a page on which users could see if certain featured streamers were currently online.  I decided to expand on it by embedding a video player directly into the site and including the top 10 currently online streamers for Starcraft II.",
+  img: "./twitchss.png",
+  href: "./TwitchViewer/twitch.html"
 }, {
   name: "Laath Martin Photography",
-  info: "My first site that I created for a friend who happens to be a skilled photographer.  He has a functioning website but I felt I could make it look a bit cleaner and it gave me a chance to apply the skills I had been learning to the real world."
+  info: "My first site that I created for a friend who happens to be a skilled photographer.  He has a functioning website but I felt I could make it look a bit cleaner and it gave me a chance to apply the skills I had been learning to the real world.",
+  img: "./laathss.png",
+  href: "./Laath/Pages/LaathMartin.html"
 }, {
   name: "Pomodoro Timer",
-  info: "Part of the Free Code Camp challenges. This project is a Pomodoro Timer, used as an effective productivity tool.  The timer runs for a period of work, then changes to a shorter timer for a break from work, then repeats the work period.  It includes a clock at the bottom and color transitions as the timer approaches the end of a cycle."
+  info: "Part of the Free Code Camp challenges. This project is a Pomodoro Timer, used as an effective productivity tool.  The timer runs for a period of work, then changes to a shorter timer for a break from work, then repeats the work period.  It includes a clock at the bottom and color transitions as the timer approaches the end of a cycle.",
+  img: "./pomodoross.png",
+  href: "./Pomodoro/Pomodoro.html"
 }, {
   name: "Javascript Calculator",
-  info: "This is a fully functional calculator made with Javascript.  The calculator is designed to look like a Texas Instruments TI-108 calculator which were what I used in grade school."
+  info: "This is a fully functional calculator made with Javascript.  The calculator is designed to look like a Texas Instruments TI-108 calculator which were what I used in grade school.",
+  img: "./calculatorss.png",
+  href: "./Calculator/Calculator.html"
 }, {
   name: "Simon",
-  info: "This project is a version of the old Simon Says game which repeats a color and sound pattern that the player needs to match.  It goes up to 20 elements in the pattern before the player wins."
+  info: "This project is a version of the old Simon Says game which repeats a color and sound pattern that the player needs to match.  It goes up to 20 elements in the pattern before the player wins.",
+  img: "./simonss.png",
+  href: "./Simon/Simon.html"
 }];
 
 function projectPop (){
-
-
+  var workId = 1;
+  $(".worklist").click(function() {
+    $("#workInfo").empty();
+    $("#workImg").empty();
+    $("#workTitle").empty();
+    workId = Number($(this).attr("id"));
+    $("#workTitle").append("<h3>" + projects[workId - 1].name + "</h3>");
+    $("#workImg").append("<a target='_blank' href=" + projects[workId - 1].href + "><img src=" + projects[workId - 1].img + "></a><p>Click image to see full project</p>");
+    $("#workInfo").append("<h5>" + projects[workId - 1].info + "</h5>");
+  });
 }
 
 
@@ -57,6 +77,43 @@ function sectionActive(){
     $(".brand").addClass("active");
   });
 }
+function downArrowClick (){
+  $(".downArrow").click(function() {
+    $("html, body").animate({
+      scrollTop: $("#about").offset().top
+    }, "slow");
+  });
+  $(".downArrowAbout").click(function() {
+    $("html, body").animate({
+      scrollTop: $("#work").offset().top
+    }, "slow");
+  });
+  $(".downArrowWork").click(function() {
+    $("html, body").animate({
+      scrollTop: $("#contact").offset().top
+    }, "slow");
+  });
+  $(".aboutMenu").click(function() {
+    $("html, body").animate({
+      scrollTop: $("#about").offset().top
+    }, "slow");
+  });
+  $(".workMenu").click(function() {
+    $("html, body").animate({
+      scrollTop: $("#work").offset().top
+    }, "slow");
+  });
+  $(".contactMenu").click(function() {
+    $("html, body").animate({
+      scrollTop: $("#contact").offset().top
+    }, "slow");
+  });
+  $(".brand").click(function(){
+    $("html, body").animate({
+      scrollTop: $("#home").offset().top
+    }, "slow");
+  });
+}
 
 function workActive () {
   $(".worklist").click(function(){
@@ -64,6 +121,16 @@ function workActive () {
     $(this).addClass("active");
   });
 }
+
+/*$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if(scroll > 510){
+      $(".brand").removeClass("active");
+      $(".aboutMenu").addClass("active");
+    }
+    console.log(scroll);
+  });*/
+
 
 $(document).ready(function(){
   $("#menubars").click(function(){
@@ -74,9 +141,10 @@ $(document).ready(function(){
     }
   });
 
+  downArrowClick();
   projectPop();
-  sectionActive();
   workActive();
+  sectionActive();
 
   $(".fa-times").click(function(){
     menuClose();
